223a804a74b794ed64e553c2f98288cd5b93df4d
evgeniagrebe5-eng <evgeniagrebe5@gmail.com>
2026-09-10 01:16:12 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -256,6 +256,7 @@ async def callback_handler(update, context):
             f"💳 Оплата пакета: {title}\n"
             f"Сумма: {price} ₸\n\n"
             "Переведи указанную сумму на Kaspi.\n"
+            "Номер для перевода: +7 777 878 00 78\n\n"
             "После оплаты нажми кнопку ниже 👇",
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton(
