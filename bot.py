eb4e8f3ea39076c9f486f9907c1a512646422203
evgeniagrebe5-eng <evgeniagrebe5@gmail.com>
2026-09-11 00:09:17 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -530,7 +530,7 @@ async def photo_handler(update, context):
 
     
 
-      if is_admin(update) and context.user_data.get("admin_state") == "channel_photo":
+        if is_admin(update) and context.user_data.get("admin_state") == "channel_photo":
         key = context.user_data.get("channel_style")
         caption = context.user_data.get("channel_caption", "")
 
