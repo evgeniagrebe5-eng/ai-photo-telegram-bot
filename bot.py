4a760e40db923cb2dd0e96c1558af58f21073ee0
evgeniagrebe5-eng <evgeniagrebe5@gmail.com>
2026-09-11 00:31:56 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -451,7 +451,7 @@ if data.startswith("gallery:"):
         return
 
     if data.startswith("style:"):
-    key = data.split(":", 1)[1]
+       key = data.split(":", 1)[1]
 
     if key not in SESSIONS:
         await query.message.reply_text(
