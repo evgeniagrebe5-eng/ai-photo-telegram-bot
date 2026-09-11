8eb44d0c6d939dbe8d1423c5700cbfbb5bf59021
evgeniagrebe5-eng <evgeniagrebe5@gmail.com>
2026-09-12 00:37:37 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -664,6 +664,13 @@ async def photo_handler(update, context):
 
         return
 
+    style = context.user_data.get("selected_style")
+             if not style or style not in SESSIONS:
+              await update.message.reply_text(
+               "❌ Сначала выбери фотосессию.",
+               reply_markup=client_keyboard()
+    )
+    return
     session = SESSIONS[style]
     await update.message.reply_text("📸 Фото получила!\n\n✨ Начинаю обработку...\nЭто может занять некоторое время.")
 
